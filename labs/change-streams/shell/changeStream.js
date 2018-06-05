06c84fd3d36ea31968df165b536414b9fb0b69a6
Ravi Kalia <ravkalia@gmail.com>
2018-06-05 14:09:34 -0500

    workshop follow through until stream observables

--- labs/change-streams/shell/changeStream.js
+++ labs/change-streams/shell/changeStream.js
@@ -1,26 +1,34 @@
-conn = new Mongo("mongodb://localhost:27017/demo?replicaSet=rs");
-db = conn.getDB("demo");
+conn = new Mongo('mongodb://localhost:27017/demo?replicaSet=rs');
+db = conn.getDB('demo');
 collection = db.stock;
 
 const insertOps = {
-  $match: { operationType: "insert" }
+  $match: { operationType: 'insert' },
 };
 
-const updateOps=null;
+const updateOps = null;
 
 const changeStreamCursor = collection.watch([
-  csFilter === 0 ? insertOps : updateOps
+  csFilter === 0 ? insertOps : updateOps,
 ]);
 
 pollStream(changeStreamCursor);
-//resumeStream(changeStreamCursor, true);
+// resumeStream(changeStreamCursor, true);
 
-//INSERT pollStream() FUNCTION HERE
-
-//INSERT resumeStream() FUNCTION HERE
+const pollStream = (cursor) => {
+  while (!cursor.isExhausted()) {
+    if (cursor.hasNext()) {
+      const change = cursor.next();
+      // this is where you should insert your change stream processing logic - in this demo example, we're simply printing the change stream JSON document to the console
+      print(JSON.stringify(change, null, 2));
+    }
+  }
+  pollStream(cursor);
+};
+// INSERT resumeStream() FUNCTION HERE
 
 function sleepFor(sleepDuration) {
-  var now = new Date().getTime();
+  const now = new Date().getTime();
   while (new Date().getTime() < now + sleepDuration) {
     /* do nothing */
   }
--- labs/change-streams/shell/startRS.sh
+++ labs/change-streams/shell/startRS.sh
@@ -1,2 +1,2 @@
 mkdir -p data/rs1/db
-mongod --dbpath ./data/rs1/db --replSet "rs" --port 27017
+mongod --dbpath ./data/rs1/db --replSet "rs" --port 27118
